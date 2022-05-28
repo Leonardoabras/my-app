4ec93d20acc5f43da493665ee4cf077e4cbee5cd
Leonardo Abras <leonardoabras@hotmail.com>
2022-05-27 21:11:39 -0300

    Criando classe de diretivas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimeiroComponenteComponent } from './componentes/primeiro-componente/primeiro-componente.component';
 import { DadoPaiComponent } from './componentes/dado-pai/dado-pai.component';
+import { DiretivasComponent } from './componentes/diretivas/diretivas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrimeiroComponenteComponent,
-    DadoPaiComponent
+    DadoPaiComponent,
+    DiretivasComponent
   ],
   imports: [
     BrowserModule,
